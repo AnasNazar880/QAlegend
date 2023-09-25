86d40e12ccb42b388d9561f7358ae20bbe188535
AnasNazar880 <102611445+AnasNazar880@users.noreply.github.com>
2023-09-25 20:12:02 +0530

    changes in the reset page

--- src/test/java/com/qalegend/testscripts/ResetPageTest.java
+++ src/test/java/com/qalegend/testscripts/ResetPageTest.java
@@ -3,4 +3,7 @@ package com.qalegend.testscripts;
 import com.qalegend.automationcore.Base;
 
 public class ResetPageTest extends Base {
+    public void  givemyname(){
+        System.out.println("i am anas");
+    }
 }
